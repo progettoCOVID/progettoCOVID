05606b0ced1dce615b5bb6611a446b7ff0acf173
Trapani Giosuè <giosuetrap2002@gmail.com>
2021-02-01 15:59:54 +0100

    Finished Idroxiclorochina

--- controllers/basicController.js
+++ controllers/basicController.js
@@ -2,19 +2,18 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('./db/tabella.db')
 const charts = require('../fakedbchart');
+const app = express()
 
+//idrox
 var nslDate = [{}]
 db.serialize(() => {
     let i = 0;
     db.each("SELECT prs_date, prs_nsl_num FROM Prescrizioni INNER JOIN Farmaci ON Farmaci.frmc_num = Prescrizioni.prs_frmc_id WHERE Farmaci.frmc_atc5 = 'IDROXICLOROCHINA'", (err, row) => {
         i++;
-        nslDate.push('{date: ' + row['prs_date'].split(' ')[0] + ", noso: " + row['prs_nsl_num'] + '}')
+        nslDate.push(row['prs_date'].split(' ')[0] + "," + row['prs_nsl_num'])
     })
 })
 db.close();
-
-
-const app = express()
 
 const chartsArray = [];
 for(const chart in charts) {
@@ -22,12 +21,16 @@ for(const chart in charts) {
 }
 
 exports.get_home = (req, res) => {
+    res.render("home", {id: req.params.id, charts: chartsArray})
     
 }
 
 exports.get_charts = (req, res) => {
-    var nslDateString = JSON.stringify(Object.assign({}, nslDate))
-    var nslDateJSON = JSON.parse(nslDateString)
-    console.log(nslDateJSON);
-    res.render("chart", {id: req.params.id, charts: chartsArray, data: nslDateJSON});
+    const data = get_idrox();
+    res.render("chart", {id: req.params.id, charts: chartsArray, data: data});
+}
+
+const get_idrox = () => {
+    var nslDateString = JSON.stringify({nslDate})
+    return nslDateString
 }
--- public/js/script.js
+++ public/js/script.js
@@ -1,51 +1,52 @@
 let chartData = document.getElementById("chart-data").dataset.chartdata;
-chartData = chartData.split('[object Object],')[1];
-chartData = JSON.stringify({chartData})
 chartData = JSON.parse(chartData)
-console.log(chartData)
-// console.log(typeof chartData, chartData);
-
+chartData = Object.values(chartData['nslDate'])
 
 // Gennaio, Febbraio, Marzo, Aprile, Maggio, Giugno, Luglio, Agosto, Settembre, Ottobre, Novembre, Dicembre
 const months = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
 const months_restr = ['GEN', 'FEB', 'MAR', 'APR', 'MAG', 'GIU', 'LUG', 'AGO', 'SET', 'OTT', 'NOV', 'DIC']
 const days_in_months = ['31', '29', '31', '30', '31', '30', '31', '31', '30', '31', '30', '31'];
 
-
-
 let dataX = [];
 chartData.forEach(el => {
+    el = el + ''
+    el = el.split(",")
+
     let tmp = el[0].split('-');
     // giorno mese - 20 || es. 15 Luglio - 20
     let month = months[months_restr.indexOf(tmp[1])];
     // let day = (tmp[0] < 10)? '0'+tmp[0] : tmp[0];
     dataX.push(`${tmp[0]}-${month}-${tmp[2]}20`);
 })
-// console.log(dataX);
+dataX.splice(0, 1)
+dataX = [... new Set(dataX)]
 
-
-
-var xEle = [];
-
-days_in_months.forEach((ele, index) => {
-    for (let i=1; i<=ele; i++) {
-        let day = (i < 10)? '0'+i : i;
-        xEle.push(`${day}-${months[index]}-2020`);
-        /*if(i==1 || i==15){
-            xEle.push(`${i} ${months[index]} - 20`);
-        }*/
-    }
+let dataY = []
+for(i = 0; i < dataX.length; i++){
+    dataY.push(0)
+}
+console.log(dataY.length + ' - ' + dataX.length)
+dataX.forEach(dx => {
+    chartData.forEach(el => {
+        el = el + ''
+        el = el.split(",")
+        let tmp = el[0].split('-')
+        let month = months[months_restr.indexOf(tmp[1])];
+        if(JSON.stringify(dx) === JSON.stringify(`${tmp[0]}-${month}-${tmp[2]}20`)){
+            dataY[dataX.indexOf(dx)] += 1
+        }
+    })
 })
+console.log(dataY)
 
 // console.log(xEle);
-
 var dates = [];
-dataX.forEach(date => {
+for(i=0; i < dataX.length; i++){
     let obj = {};
-    obj.x = new Date(date.split('-')[2], date.split('-')[1], date.split('-')[0]);
-    obj.y = Math.floor(Math.random() * (30 - 1) + 1);
+    obj.x = new Date(dataX[i].split('-')[2], dataX[i].split('-')[1], dataX[i].split('-')[0]);
+    obj.y = dataY[i]
     dates.push(obj);
-})
+}
 
 
 const ctx = document.getElementById("chart").getContext("2d");
@@ -53,9 +54,8 @@ var dates2 = dates.sort((a,b) => b.x - a.x);
 const chart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: xEle,
         datasets: [{
-            label: 'My First dataset',
+            label: 'Idroxiclorochina',
             borderColor: 'red',
             backgroundColor: 'red',
             fill: false,
