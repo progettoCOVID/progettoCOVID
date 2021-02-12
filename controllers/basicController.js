11e66a2ecdde77888ad1cafd92cd6a2553675642
Trapani Giosuè <giosuetrap2002@gmail.com>
2021-02-12 19:37:26 +0100

    Added glico and ossigeno

--- controllers/basicController.js
+++ controllers/basicController.js
@@ -6,11 +6,27 @@ const app = express()
 
 //idrox
 var nslDate = [{}]
+var nslDateGlico = [{}]
+var nslDateOssigeno = [{}]
 db.serialize(() => {
     let i = 0;
-    db.each("SELECT prs_date, prs_nsl_num FROM Prescrizioni INNER JOIN Farmaci ON Farmaci.frmc_num = Prescrizioni.prs_frmc_id WHERE Farmaci.frmc_atc5 = 'IDROXICLOROCHINA'", (err, row) => {
+    db.each("SELECT prs_date, prs_nsl_num " +
+                "FROM Prescrizioni INNER JOIN Farmaci ON Farmaci.frmc_num = Prescrizioni.prs_frmc_id " +
+                "WHERE Farmaci.frmc_atc5 = 'IDROXICLOROCHINA'", (err, row) => {
         i++;
         nslDate.push(row['prs_date'].split(' ')[0] + "," + row['prs_nsl_num'])
+    })
+    db.each("SELECT prs_date, prs_nsl_num " +
+                "FROM Prescrizioni INNER JOIN Farmaci ON Farmaci.frmc_num = Prescrizioni.prs_frmc_id " +
+                "WHERE Farmaci.frmc_atc5 LIKE 'Desamentasone%' OR Farmaci.frmc_atc5 LIKE '%predniso%'", (err, row) => {
+        i++;
+        nslDateGlico.push(row['prs_date'].split(' ')[0] + "," + row['prs_nsl_num'])
+    })
+    db.each("SELECT prs_date, prs_nsl_num " +
+                "FROM Prescrizioni INNER JOIN Farmaci ON Farmaci.frmc_num = Prescrizioni.prs_frmc_id " +
+                "WHERE Farmaci.frmc_atc5 LIKE '%ossigeno%'", (err, row) => {
+        i++;
+        nslDateOssigeno.push(row['prs_date'].split(' ')[0] + "," + row['prs_nsl_num'])
     })
 })
 db.close();
@@ -26,11 +42,27 @@ exports.get_home = (req, res) => {
 }
 
 exports.get_charts = (req, res) => {
-    const data = get_idrox();
-    res.render("chart", {id: req.params.id, charts: chartsArray, data: data});
+    const dataIdrox = get_idrox();
+    const dataGlico = get_glico();
+    const dataOssigeno = get_ossigeno();
+    res.render("chart", { id: req.params.id, 
+                            charts: chartsArray, 
+                            dataIdrox: dataIdrox,
+                            dataGlico: dataGlico,
+                            dataOssigeno: dataOssigeno});
 }
 
 const get_idrox = () => {
     var nslDateString = JSON.stringify({nslDate})
     return nslDateString
+}
+
+const get_glico = () => {
+    var nslDateStringGlico = JSON.stringify({nslDateGlico})
+    return nslDateStringGlico
+}
+
+const get_ossigeno = () => {
+    var nslDateStringOssigeno = JSON.stringify({nslDateOssigeno})
+    return nslDateStringOssigeno
 }
--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,27 @@
-let chartData = document.getElementById("chart-data").dataset.chartdata;
-chartData = JSON.parse(chartData)
-chartData = Object.values(chartData['nslDate'])
+let chartDataIdrox = document.getElementById("chart-data").dataset.chartidrox;
+let chartDataGlico = document.getElementById("chart-data").dataset.chartglico;
+let chartDataOssigeno = document.getElementById("chart-data").dataset.chartossigeno;
+
+chartDataIdrox = JSON.parse(chartDataIdrox)
+chartDataIdrox = Object.values(chartDataIdrox['nslDate'])
+
+chartDataGlico = JSON.parse(chartDataGlico)
+chartDataGlico = Object.values(chartDataGlico['nslDateGlico'])
+
+chartDataOssigeno = JSON.parse(chartDataOssigeno)
+chartDataOssigeno = Object.values(chartDataOssigeno['nslDateOssigeno'])
 
 // Gennaio, Febbraio, Marzo, Aprile, Maggio, Giugno, Luglio, Agosto, Settembre, Ottobre, Novembre, Dicembre
 const months = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
 const months_restr = ['GEN', 'FEB', 'MAR', 'APR', 'MAG', 'GIU', 'LUG', 'AGO', 'SET', 'OTT', 'NOV', 'DIC']
 const days_in_months = ['31', '29', '31', '30', '31', '30', '31', '31', '30', '31', '30', '31'];
 
-let dataX = [];
-chartData.forEach(el => {
+
+
+
+/* IDROX X */
+let dataXIdrox = [];
+chartDataIdrox.forEach(el => {
     el = el + ''
     el = el.split(",")
 
@@ -16,41 +29,140 @@ chartData.forEach(el => {
     // giorno mese - 20 || es. 15 Luglio - 20
     let month = months[months_restr.indexOf(tmp[1])];
     // let day = (tmp[0] < 10)? '0'+tmp[0] : tmp[0];
-    dataX.push(`${tmp[0]}-${month}-${tmp[2]}20`);
+    dataXIdrox.push(`${tmp[0]}-${month}-${tmp[2]}20`);
 })
-dataX.splice(0, 1)
-dataX = [... new Set(dataX)]
+dataXIdrox.splice(0, 1)
+dataXIdrox = [... new Set(dataXIdrox)]
 
-let dataY = []
-for(i = 0; i < dataX.length; i++){
-    dataY.push(0)
+/* IDROX Y */
+let dataYIdrox = []
+for(i = 0; i < dataXIdrox.length; i++){
+    dataYIdrox.push(0)
 }
-console.log(dataY.length + ' - ' + dataX.length)
-dataX.forEach(dx => {
-    chartData.forEach(el => {
+console.log(dataYIdrox.length + ' - ' + dataXIdrox.length)
+dataXIdrox.forEach(dx => {
+    chartDataIdrox.forEach(el => {
         el = el + ''
         el = el.split(",")
         let tmp = el[0].split('-')
         let month = months[months_restr.indexOf(tmp[1])];
         if(JSON.stringify(dx) === JSON.stringify(`${tmp[0]}-${month}-${tmp[2]}20`)){
-            dataY[dataX.indexOf(dx)] += 1
+            dataYIdrox[dataXIdrox.indexOf(dx)] += 1
         }
     })
 })
-console.log(dataY)
 
-// console.log(xEle);
-var dates = [];
-for(i=0; i < dataX.length; i++){
+// IDROX
+var datesIdrox = [];
+for(i=0; i < dataXIdrox.length; i++){
     let obj = {};
-    obj.x = new Date(dataX[i].split('-')[2], dataX[i].split('-')[1], dataX[i].split('-')[0]);
-    obj.y = dataY[i]
-    dates.push(obj);
+    obj.x = new Date(dataXIdrox[i].split('-')[2], dataXIdrox[i].split('-')[1], dataXIdrox[i].split('-')[0]);
+    obj.y = dataYIdrox[i]
+    datesIdrox.push(obj);
 }
 
 
+
+
+
+/* GLICO X */
+let dataXGlico = [];
+chartDataGlico.forEach(el => {
+    el = el + ''
+    el = el.split(",")
+
+    let tmp = el[0].split('-');
+    // giorno mese - 20 || es. 15 Luglio - 20
+    let month = months[months_restr.indexOf(tmp[1])];
+    // let day = (tmp[0] < 10)? '0'+tmp[0] : tmp[0];
+    dataXGlico.push(`${tmp[0]}-${month}-${tmp[2]}20`);
+})
+dataXGlico.splice(0, 1)
+dataXGlico = [... new Set(dataXGlico)]
+
+/* GLICO Y */
+let dataYGlico = []
+for(i = 0; i < dataXGlico.length; i++){
+    dataYGlico.push(0)
+}
+console.log(dataYGlico.length + ' - ' + dataXGlico.length)
+dataXGlico.forEach(dx => {
+    chartDataGlico.forEach(el => {
+        el = el + ''
+        el = el.split(",")
+        let tmp = el[0].split('-')
+        let month = months[months_restr.indexOf(tmp[1])];
+        if(JSON.stringify(dx) === JSON.stringify(`${tmp[0]}-${month}-${tmp[2]}20`)){
+            dataYGlico[dataXGlico.indexOf(dx)] += 1
+        }
+    })
+})
+
+// GLICO
+var datesGlico = [];
+for(i=0; i < dataXGlico.length; i++){
+    let obj = {};
+    obj.x = new Date(dataXGlico[i].split('-')[2], dataXGlico[i].split('-')[1], dataXGlico[i].split('-')[0]);
+    obj.y = dataYGlico[i]
+    datesGlico.push(obj);
+}
+
+
+
+
+
+/* OSSIGENO X */
+let dataXOssigeno = [];
+chartDataOssigeno.forEach(el => {
+    el = el + ''
+    el = el.split(",")
+
+    let tmp = el[0].split('-');
+    // giorno mese - 20 || es. 15 Luglio - 20
+    let month = months[months_restr.indexOf(tmp[1])];
+    // let day = (tmp[0] < 10)? '0'+tmp[0] : tmp[0];
+    dataXOssigeno.push(`${tmp[0]}-${month}-${tmp[2]}20`);
+})
+dataXOssigeno.splice(0, 1)
+dataXOssigeno = [... new Set(dataXOssigeno)]
+
+/* OSSIGENO Y */
+let dataYOssigeno = []
+for(i = 0; i < dataXOssigeno.length; i++){
+    dataYOssigeno.push(0)
+}
+console.log(dataYOssigeno.length + ' - ' + dataXOssigeno.length)
+dataXOssigeno.forEach(dx => {
+    chartDataOssigeno.forEach(el => {
+        el = el + ''
+        el = el.split(",")
+        let tmp = el[0].split('-')
+        let month = months[months_restr.indexOf(tmp[1])];
+        if(JSON.stringify(dx) === JSON.stringify(`${tmp[0]}-${month}-${tmp[2]}20`)){
+            dataYOssigeno[dataXOssigeno.indexOf(dx)] += 1
+        }
+    })
+})
+
+// OSSIGENO
+var datesOssigeno = [];
+for(i=0; i < dataXOssigeno.length; i++){
+    let obj = {};
+    obj.x = new Date(dataXOssigeno[i].split('-')[2], dataXOssigeno[i].split('-')[1], dataXOssigeno[i].split('-')[0]);
+    obj.y = dataYOssigeno[i]
+    datesOssigeno.push(obj);
+}
+
+
+
+
+
+
+
 const ctx = document.getElementById("chart").getContext("2d");
-var dates2 = dates.sort((a,b) => b.x - a.x);
+var dates2Idrox = datesIdrox.sort((a,b) => b.x - a.x);
+var dates2Glico = datesGlico.sort((a,b) => b.x - a.x);
+var dates2Ossigeno = datesOssigeno.sort((a,b) => b.x - a.x);
 const chart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -59,8 +171,22 @@ const chart = new Chart(ctx, {
             borderColor: 'red',
             backgroundColor: 'red',
             fill: false,
-            data: dates2
-         }]
+            data: dates2Idrox
+         },
+         {
+            label: 'Glicorticoidi',
+            borderColor: 'yellow',
+            backgroundColor: 'yellow',
+            fill: false,
+            data: dates2Glico
+         },
+        {
+            label: 'Ossigeno',
+            borderColor: 'lightblue',
+            backgroundColor: 'lightblue',
+            fill: false,
+            data: dates2Ossigeno
+        }],
     },
     options: {
         responsive: true,
