6c22eab26440fc0781fc78b4149d7b590700c3b3
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2021-02-26 20:36:07 +0100

    eparine NOT completed, 8000 in a day?

--- controllers/eparineController.js
+++ controllers/eparineController.js
@@ -4,9 +4,9 @@ const db = new sqlite3.Database('./db/tabella.db');
 let tmpData = [{}];
 var nslDateEparine = [{}];
 db.serialize(() => {
-    db.each("SELECT prs_nsl_num, prs_date, prs_nsl_num, prs_dos " +
+    db.each("SELECT prs_nsl_num, prs_date, prs_dos " +
         "FROM Prescrizioni INNER JOIN Farmaci ON Farmaci.frmc_num = Prescrizioni.prs_frmc_id " +
-        "WHERE Farmaci.frmc_atc4 LIKE 'eparin%'", (err, row) => {
+        "WHERE Farmaci.frmc_atc4 LIKE 'eparin%' ORDER BY prs_date, prs_nsl_num", (err, row) => {
             // nslDateEparine.push(row['prs_date'].split(' ')[0] + "," + row['prs_nsl_num'])
             // nslDateEparine.push(row['prs_date'].split(' ')[0] + "," + row['prs_nsl_num'] + ", " + row['prs_dos'].split(' ')[0]);
             let r = {};
@@ -20,13 +20,39 @@ db.serialize(() => {
 db.close();
 
 exports.get_dates = () => {
-    nslDateEparine.forEach(el => {
-        console.log(el);
-    })
 
-    tmpData.forEach(el => {
-        nslDateEparine.push(el.prs_date + ", " + el.prs_nsl_num + ", " + el.prs_dos);
-    })
+    // console.log(tmpData.length);
+    
+    let i = 0;
+    let sum = 0;
+    while (i<tmpData.length) {
+        sum += tmpData[i].prs_dos * 1;
+        console.log(tmpData[i].prs_nsl_num + ' ' + sum + " AA")
+
+        if (tmpData[i+1] != undefined) {
+            if (tmpData[i].prs_nsl_num != tmpData[i+1].prs_nsl_num) {
+                if (sum >= 8000) {
+                    nslDateEparine.push(tmpData[i].prs_date + "," + tmpData[i].prs_nsl_num);
+                }
+
+                sum = 0;
+            }
+            
+        } else {
+            if (sum >= 8000) {
+                nslDateEparine.push(tmpData[i].prs_date + "," + tmpData[i].prs_nsl_num);
+            }
+            sum = 0;
+        }
+        i++;
+    }
+
+  
+    /* tmpData.forEach(el => {
+        if (el.prs_date != undefined && el.prs_nsl_num != undefined)
+            nslDateEparine.push(el.prs_date + "," + el.prs_nsl_num);
+    }) */
+
 
 
     return JSON.stringify({
